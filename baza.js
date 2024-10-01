cb2cdfae623b8d609b469ddb28b95ffa967e226f
Marcin Mularczyk <102852305+Klemingway@users.noreply.github.com>
2024-10-01 10:18:43 +0000

    musztarda'

---
+++ baza.js
@@ -0,0 +1,23 @@
+var mysql = require("mysql");
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "mariadb",
+    password: "mariadb",
+    database: "mariadb"
+})
+
+
+var sql = "CREATE TABLE users(test int)"
+connection.query(sql)
+    
+connection.query("SHOW TABLES", function(err,rows){
+    if(console.log(err)){
+        console.log(err)
+    }
+    else{
+        console.log(rows)
+    }
+})
+connection.end()
